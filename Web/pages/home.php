e8b1d9b365794beaa8816537c99194ff3f0a3908
chacal1231 <jedmacmahonve@unal.edu.co>
2018-02-11 12:02:35 -0500

    Added dashboard

--- Web/pages/home.php
+++ Web/pages/home.php
@@ -1,27 +1,34 @@
 <script type="text/javascript" src="http://static.fusioncharts.com/code/latest/fusioncharts.js"></script>
 <?php
+if(isset($_POST['simpan'])){
+}else{
+
+}
+?>
+
+<?php
 $Date = date ('Y-m-d');
 #Temperature query
-$TempQuery = mysqli_query($link,"SELECT TMP,hour from minutedata ORDER BY hour DESC");
+$TempQuery = mysqli_query($link,"SELECT TMP,hour from tmp ORDER BY id DESC");
 $TMP = mysqli_fetch_row($TempQuery);
 
 #Waterflow query
-$WaterQuery = mysqli_query($link,"SELECT WFR,hour FROM minutedata ORDER BY hour DESC");
-$WFR = mysqli_fetch_row($WaterQuery);
+$WaterQuery = mysqli_query($link,"SELECT ph,hour FROM ph ORDER BY id DESC");
+$ph = mysqli_fetch_row($WaterQuery);
 
 #OilFlow query
-$OilQuery = mysqli_query($link,"SELECT OFR,hour FROM minutedata ORDER BY hour DESC");
-$OFR = mysqli_fetch_row($OilQuery);
+//$OilQuery = mysqli_query($link,"SELECT OFR,hour FROM minutedata ORDER BY hour DESC");
+//$OFR = mysqli_fetch_row($OilQuery);
 
 #GasFlow query
-$GasQuery = mysqli_query($link,"SELECT GFR,hour FROM minutedata ORDER BY hour DESC");
-$GFR = mysqli_fetch_row($GasQuery);
+//$GasQuery = mysqli_query($link,"SELECT GFR,hour FROM minutedata ORDER BY hour DESC");
+//$GFR = mysqli_fetch_row($GasQuery);
 ?>
 
 <div class="row">
     <div class="col-md-3">
         <div class="mini-stat clearfix">
-            <font size="1"><b>Process Temp</b></font>
+            <font size="1"><b>Temp</b></font>
             <span class="mini-stat-icon green"><i class="fa fa-thermometer-half"></i></span>
             <div class="mini-stat-info">
                <button type="button" class="btn btn-danger btn-xs"><?=$TMP[0]?> °C</button><font size="2"></font><br>
@@ -30,39 +37,97 @@ $GFR = mysqli_fetch_row($GasQuery);
     </div>
     <div class="col-md-3">
         <div class="mini-stat clearfix">
-            <font size="1"><b>WaterFlow</b></font>
+            <font size="1"><b>pH</b></font>
             <span class="mini-stat-icon tar"><i class="fa fa-shower"></i></span>
             <div class="mini-stat-info">
-                <button type="button" class="btn btn-info btn-xs"><?=$WFR[0]?> </button><font size="2"> (Sm3/d)</font><br>
+                <button type="button" class="btn btn-info btn-xs"><?=$ph[0]?> </button><font size="2"></font><br>
              
             </div>
         </div>
     </div>
     <div class="col-md-3">
         <div class="mini-stat clearfix">
-            <font size="1"><b>OilFlow</b></font> 
+            <font size="1"><b>Ammonium</b></font> 
             <span class="mini-stat-icon pink"><i class="fa fa-tint"></i></span>
             <div class="mini-stat-info">
-            <button type="button" class="btn btn-info btn-xs"><?=$OFR[0]?></button><font size="2"> (Sm3/d)</font><br>
+            <button type="button" class="btn btn-info btn-xs"><?=$OFR[0]?></button><font size="2"> (µmol/L)</font><br>
             </div>
         </div>
     </div>
     <div class="col-md-3">
         <div class="mini-stat clearfix">
-            <font size="1"><b>GasFlow</b></font>
+            <font size="1"><b>Nitrates</b></font>
             <span class="mini-stat-icon green"><i class="fa fa-soundcloud"></i></span>
             <div class="mini-stat-info">
-                <button type="button" class="btn btn-info btn-xs"><?=$GFR[0]?></button><font size="2"> (Sm3/d)</font>
+                <button type="button" class="btn btn-info btn-xs"><?=$GFR[0]?></button><font size="2"> (mg/l)</font>
                 <br>
             </div>
         </div>
     </div>
 </div>
+<?php
+$QueryFood = mysqli_query($link,"SELECT * FROM food ORDER BY id DESC");
+$row = mysqli_fetch_array($QueryFood);
+?>
  <div class="row">
+    <div class="col-sm-12">
+        <section class="panel">
+            <header class="panel-heading">
+                Feed scheduler 
+            </header>
+            <section class="panel">
+                        <div class="panel-body"> 
+                            <td ><a target="_blank" href="pages/Enviar.php?c=1" class="btn btn-info" title="Feed fishes now!">Feed now</a></td>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                                <i class="fa fa-plus-square"></i> Add schedule 
+                            </button>
+                            <hr/>
+                            <div class="table-responsive">
+                                <table  class="display table table-bordered table-striped" id="dynamic-table">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Feed type</th>
+                                            <th>When</th>
+                                            <th>Repeat</th>
+                                            <th>Amount (Grs)</th>
+                                            <th><i class="fa fa-pencil"></i></th>
+                                            <th><i class="fa fa-trash-o"></i></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach( $QueryFood as $row => $field ) : ?> <!-- Mulai loop -->
+                                        <tr class="text-besar">
+                                            <td><?php echo $field['id']; ?></td>
+                                            <td><?php echo $field['food']; ?></td>
+                                            <td><?php echo $field['time']; ?></td>
+                                            <td><?php echo $field['frec']; ?></td>
+                                            <td><?php echo $field['grs']; ?></td>
+                                            <td>
+                                                <a class="btn btn-success btn-xs" href="?page=modi_cli&id=<?php echo $field['id']; ?>" title="Modificar">
+                                                    <i class="fa fa-pencil"></i>
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <a class="btn btn-danger btn-xs" href="?page=eli_cli&id=<?php echo $field['id']; ?>&ipp=<?php echo $field['ip']; ?>" title="Eliminar" onClick="return confirm('Desea eliminar al cliente ?...')">
+                                                    <i class="fa fa-trash-o"></i>
+                                                </a>
+                                            </td>
+                                                
+                                        </tr>
+                                        <?php endforeach; ?> <!-- Selesai loop -->                                  
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </section>
+        </div>
+    </section>
+</div>
     <div class="col-sm-6">
         <section class="panel">
             <header class="panel-heading">
-               Process Temperature
+               Temperature
             </header>
             <div class="panel-body"> 
                 <div class="table-responsive">
@@ -94,7 +159,7 @@ window.onload = function () {
                 var chart = new CanvasJS.Chart("chart", {
                     zoomEnabled: true,
                     title: {
-                        text: "Process Temperature"
+                        text: "Temperature"
                     },
                     axisX: {
                         title: "chart updates every " + 60 + " secs"
@@ -137,19 +202,19 @@ window.onload = function () {
                 var chart2 = new CanvasJS.Chart("chart2", {
                     zoomEnabled: true,
                     title: {
-                        text: "Oil Flow Rate"
+                        text: "Nitrates "
                     },
                     axisX: {
                         title: "chart updates every " + 60 + " secs"
                     },
                     axisY: {
-                        title: "Sm3/d",
+                        title: "mg/l",
 
-                        suffix: " Sm3/d",
+                        suffix: " mg/l",
 
                     },
                     data: [{type: "line",
-                             toolTipContent: "{label} : {y} Sm3/d",
+                             toolTipContent: "{label} : {y} mg/l",
                              lineColor: "red", 
 
                             dataPoints: data2}],
@@ -163,9 +228,10 @@ window.onload = function () {
 </script>
 </head>
 <body>
-<div id="chart" style="height: 370px; max-width: 920px; margin: 0px auto;""></div>
+<div id="chart" style="height: 370px; width: 100%;"></div>
 <script src="backend/js/canvasjs.min.js"></script>
 </body>
+</head>
 </html>     
                     </table>
                 </div>
@@ -176,15 +242,86 @@ window.onload = function () {
         <div class="col-sm-6">
         <section class="panel">
             <header class="panel-heading">
-                Oil Flow rate 
+                Nitrates 
             </header>
             <div class="panel-body"> 
                 <div class="table-responsive">
-                    <div id="chart2" style="height: 370px; max-width: 920px; margin: 0px auto;""></div>
+                    <div id="chart2" style="height: 370px; width: 100%;""></div>
                     <script src="backend/js/canvasjs.min.js"></script>
-                    </table>
+                </table>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+ <!-- Modal -->
+           
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title" id="myModalLabel">New scheldue</h4>
+                            </div>
+                        <div class="modal-body">
+                        <form id="modal-form" action="" method="post">
+                            <div class="form-group">
+                                <label for="recipient-name" class="control-label"><b>Type food *:</b></label>
+                                <input type="text" class="form-control" name="type" placeholder="" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="recipient-name" class="control-label"><b>Start hour:</b></label>
+                                <select id="repeat" name="frec" class="form-control">
+                                    <option value="">-- Seleccionar --</option>
+                                    <option value="0">00:00</option>
+                                    <option value="1">01:00</option>
+                                    <option value="2">02:00</option>
+                                    <option value="3">03:00</option>
+                                    <option value="4">04:00</option>
+                                    <option value="5">05:00</option>
+                                    <option value="6">06:00</option>
+                                    <option value="7">07:00</option>
+                                    <option value="8">08:00</option>
+                                    <option value="9">09:00</option>
+                                    <option value="10">10:00</option>
+                                    <option value="11">11:00</option>
+                                    <option value="12">12:00</option>
+                                    <option value="13">13:00</option>
+                                    <option value="14">14:00</option>
+                                    <option value="15">15:00</option>
+                                    <option value="16">16:00</option>
+                                    <option value="17">17:00</option>
+                                    <option value="18">18:00</option>
+                                    <option value="19">19:00</option>
+                                    <option value="20">20:00</option>
+                                    <option value="21">21:00</option>
+                                    <option value="22">22:00</option>
+                                    <option value="23">23:00</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="recipient-name" class="control-label"><b>Repeat:</b></label>
+                                <select id="repeat" name="ciudad" class="form-control">
+                                    <option value="">-- Seleccionar --</option>
+                                    <option value="0">Everyday</option>
+                                    <option value="1">Weekends</option>
+                                    <option value="2">Monday to Friday</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="recipient-name" class="control-label"><b>Amount in gr *:</b></label>
+                                <input type="correo" class="form-control" name="amount" required/>
+                            </div>
+                            <h3><b><p class="alert alert-danger">* Campos obligatorios</b></h3>
+                            <div class="text-right">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                                <button type="submit" class="btn btn-primary" name="simpan" value="Sign up">Agregar cliente</button>
+                            </div>
+                        </form>
                     </div>
-                    </div>
-                    </section>
-                    </div>
-                    </div>
+                </div>
+            </div> 
+
+            </head>
+</html>
