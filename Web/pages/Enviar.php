377727b5d42a6423d16131b995d636ab72193a9d
chacal1231 <jedmacmahonve@unal.edu.co>
2018-02-11 12:02:25 -0500

    Modified command send for to make it work with button.

--- Web/pages/Enviar.php
+++ Web/pages/Enviar.php
@@ -3,16 +3,15 @@
 .datepicker{z-index:1151 !important;}
 </style>
 <?php
-if(isset($_POST['simpan'])){ 
 // data from module enviar datos
-$Command=$_POST['Command'];
+$Command=$_GET['c'];
 // form submitted
  
 // where is the socket server?
 $host="127.0.0.1";
-$port = 7774;
+$port = 7775;
 //echo $Command;
-$message=$Command . "\r\n";
+$message="C=". $Command . "\r\n";
  
 // create socket
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
@@ -42,41 +41,8 @@ socket_close($socket);
 
     echo "<br><br><br><br><br><br><br><br>";
     echo '<div align="center">';
-    echo "<h1>The command was send to the DAU</h1>";
+    echo "<h1>Fishes successfully fed!</h1>";
     echo "</div>";
     echo "</br></br></br></br></br></br></br></br>";
-	echo "<script>setTimeout(\"location.href = '?page=Enviar';\", 3000);</script>";
-}else{
-	echo "<body onLoad=$('#myModal').modal('show')>";
-}
+	echo "<script>setTimeout(\"location.href = '../index.php';\", 3000);</script>";
 ?>
-
- <!-- Modal -->
-           
-            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-            <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title" id="myModalLabel">Send command's to DAU </h4>
-                            </div>
-                        <div class="modal-body">
-                        <form id="modal-form" action="" method="post">
-                             <div class="form-group">
-							<label for="sel1">Select list:</label>
-							<select class="form-control" id="Command" name="Command">
-							<option value="C=1">DAU Testmode ON</option>
-							<option value="C=2">DAU Testmode OFF</option>
-  </select>
-</div> 
-							<div class="text-right">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                                <button type="submit" class="btn btn-primary" name="simpan" value="Sign up">Enviar</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div> 
-
-            </head>
-</html>
